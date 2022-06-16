969c89165f57d2339554c1278443e1504067dca7
Zen <muhzaini30@gmail.com>
2022-06-17 07:22:06 +0800

    chore: change package name

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
 export default function FromCSV(csv: string, delimiters = ","): any {
-	const splitLine: string[] = csv.split('\n')
+  const splitLine: string[] = csv.split("\n");
 
-	let content: any = []
-	let headers: string[] = []
+  let content: any = [];
+  let headers: string[] = [];
 
-	let newArray: any = []
+  let newArray: any = [];
 
-		
-	for (const index in splitLine){
-		const n: number = +index
-		if (n == 0){
-			headers = splitLine[n].split(delimiters)
-		} else {
-			content.push(splitLine[n].split(delimiters))
-		}
-	}
+  for (const index in splitLine) {
+    const n: number = +index;
+    if (n == 0) {
+      headers = splitLine[n].split(delimiters);
+    } else {
+      content.push(splitLine[n].split(delimiters));
+    }
+  }
 
-	for (const x of content){
-		let newObject = {}
-		for (const n in x){
-			newObject[headers[n]] = x[n]
-		}
-		newArray.push(newObject)
-	}
+  for (const x of content) {
+    let newObject = {};
+    for (const n in x) {
+      newObject[headers[n]] = x[n];
+    }
+    newArray.push(newObject);
+  }
 
-	return newArray
+  return newArray;
 }
